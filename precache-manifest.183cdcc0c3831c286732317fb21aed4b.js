7ddff8133636155229de34e1d52f616f16c1cf78
Rodrigue Coail <rodrigue.coail@tmtlsdw.com>
2020-07-06 00:30:43 +0200

    Updates

---
+++ precache-manifest.183cdcc0c3831c286732317fb21aed4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e8321c63248c50693e3136e99daee4",
+    "url": "/react_beginner/index.html"
+  },
+  {
+    "revision": "07426a5dd7aa297232d0",
+    "url": "/react_beginner/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a4f955c09b21d981e281",
+    "url": "/react_beginner/static/css/main.d0a4364e.chunk.css"
+  },
+  {
+    "revision": "07426a5dd7aa297232d0",
+    "url": "/react_beginner/static/js/2.7c96b5a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_beginner/static/js/2.7c96b5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f955c09b21d981e281",
+    "url": "/react_beginner/static/js/main.90458110.chunk.js"
+  },
+  {
+    "revision": "902922ca6f84821389bc",
+    "url": "/react_beginner/static/js/runtime-main.f6f8c818.js"
+  }
+]);
